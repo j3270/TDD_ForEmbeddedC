1d67c35a302de28cbe3514f276de20e1d731c8d2
Justin Jordan <jordanj@curtisinst.com>
2022-10-02 21:11:47 -0700

    Use option for selecting test framework working, see settings.json for each workspace

--- Tests/testFrameWork.cmake
+++ Tests/testFrameWork.cmake
@@ -1,14 +1,39 @@
 
-set(UNITY_INCLUDE_DIRS
-${CMAKE_CURRENT_SOURCE_DIR}/../../../Unity/src
-${CMAKE_CURRENT_SOURCE_DIR}/../../../Unity/extras/fixture/src
-${CMAKE_CURRENT_SOURCE_DIR}/../../../Unity/extras/memory/src
-)
+if(${USE_UNITY})
+    
+    message(STATUS "Unity test framework selected.")
 
-set(UNITY_EXTENSION_FIXTURE ON CACHE BOOL "enable extension fixtures")
-set(UNITY_EXTENSION_MEMORY ON CACHE BOOL "enable extension memory")
-add_subdirectory(
-    ${CMAKE_CURRENT_SOURCE_DIR}/../../../Unity
-    ${CMAKE_CURRENT_SOURCE_DIR}/build/Uinty
-    EXCLUDE_FROM_ALL
-)
+    set(UNITY_EXTENSION_FIXTURE ON CACHE BOOL "enable extension fixtures")
+    set(UNITY_EXTENSION_MEMORY ON CACHE BOOL "enable extension memory")
+
+    add_subdirectory(
+        ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity
+        ${CMAKE_CURRENT_SOURCE_DIR}/build/Uinty
+        EXCLUDE_FROM_ALL
+    )
+
+    set(UNITY_INCLUDE_DIRS
+    ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/src
+    ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/extras/fixture/src
+    ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/extras/memory/src
+    )
+
+else()
+
+    if(${USE_CPP_U_TEST})
+        message(STATUS "CppUtest test framework selected.")
+    else()
+        message(STATUS "No test framework selected, defaulting to CppUtest.")
+    endif()
+
+    add_subdirectory(
+        ${CMAKE_CURRENT_SOURCE_DIR}/../../cpputest
+        ${CMAKE_CURRENT_SOURCE_DIR}/build/cpputest
+        EXCLUDE_FROM_ALL
+    )
+
+    set(CPP_UTEST_INCLUDE_DIRS
+        ${CMAKE_CURRENT_SOURCE_DIR}/../../cpputest/include
+    )
+    
+endif()
