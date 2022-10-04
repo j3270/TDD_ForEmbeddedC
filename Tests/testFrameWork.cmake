33d892f2009cfaee3added1fe5f28c1d734e5063
Justin Jordan <jordanj@curtisinst.com>
2022-10-04 13:48:19 -0700

    Updates for building test frame work

--- Tests/testFrameWork.cmake
+++ Tests/testFrameWork.cmake
@@ -1,43 +1,47 @@
 
+function(build_test_framework arg_path_to_test_framework)
+
 if(${USE_UNITY})
 
-    if(NOT TARGET unity)
-        message(STATUS "Unity test framework selected.")
+    message(STATUS "Unity test framework selected.")
 
+    if(NOT TARGET unity)
+        
         set(UNITY_EXTENSION_FIXTURE ON CACHE BOOL "enable extension fixtures")
         set(UNITY_EXTENSION_MEMORY ON CACHE BOOL "enable extension memory")
 
         add_subdirectory(
-            ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity
-            ${CMAKE_CURRENT_SOURCE_DIR}/build/Uinty
-            EXCLUDE_FROM_ALL
+            ${arg_path_to_test_framework}
+            ${CMAKE_SOURCE_DIR}/build/Unity
         )
     endif()
 
-    set(UNITY_INCLUDE_DIRS
-        ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/src
-        ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/extras/fixture/src
-        ${CMAKE_CURRENT_SOURCE_DIR}/../../Unity/extras/memory/src
+    #includes for framework
+    set(TEST_FRAMEWORK_INCLUDES
+        ${arg_path_to_test_framework}/src
+        ${arg_path_to_test_framework}/extras/fixture/src
+        ${arg_path_to_test_framework}/extras/memory/src
+        PARENT_SCOPE
     )
 
 else()
 
+    if(${USE_CPP_U_TEST})
+        message(STATUS "CppUtest test framework selected.")
+    else()
+        message(STATUS "No test framework selected, defaulting to CppUtest.")
+    endif()
+
     if(NOT TARGET CppUTest)
-        if(${USE_CPP_U_TEST})
-            message(STATUS "CppUtest test framework selected.")
-        else()
-            message(STATUS "No test framework selected, defaulting to CppUtest.")
-        endif()
-    
         add_subdirectory(
-            ${CMAKE_CURRENT_SOURCE_DIR}/../../cpputest
-            ${CMAKE_CURRENT_SOURCE_DIR}/build/cpputest
-            EXCLUDE_FROM_ALL
+            ${arg_path_to_test_framework}
+            ${CMAKE_SOURCE_DIR}/build/CppUtest
         )
     endif()
 
-    set(CPP_UTEST_INCLUDE_DIRS
-        ${CMAKE_CURRENT_SOURCE_DIR}/../../cpputest/include
-    )
+    #includes for framework
+    set(TEST_FRAMEWORK_INCLUDES ${arg_path_to_test_framework}/include PARENT_SCOPE)
     
 endif()
+
+endfunction()
