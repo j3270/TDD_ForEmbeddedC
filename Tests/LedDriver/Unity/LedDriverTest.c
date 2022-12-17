9b1d149af3e1c902e2c26a50c50d0fe4ed85e2ff
J3270 <justinjordan1978@gmail.com>
2022-12-17 07:05:48 -0800

    add multiple leds test

--- Tests/LedDriver/Unity/LedDriverTest.c
+++ Tests/LedDriver/Unity/LedDriverTest.c
@@ -46,3 +46,10 @@ TEST(LedDriver, TurnOffLedOne)
     LedDriver_TurnOff(1);
     TEST_ASSERT_EQUAL_HEX16(0, virtualLeds);
 }
+
+TEST(LedDriver, TurnOnMultipleLeds)
+{
+    LedDriver_TurnOn(9);
+    LedDriver_TurnOn(4);
+    TEST_ASSERT_EQUAL_HEX16(0x0208, virtualLeds);
+}
