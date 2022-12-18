bc98a409cd79f8efaf757a211ea875a1607f1c05
J3270 <justinjordan1978@gmail.com>
2022-12-18 09:14:04 -0800

    add cpputest files for LedDriver and Random

---
+++ Tests/Drivers/LedDriver/CppUtest/LedDriverTest.cpp
@@ -0,0 +1,27 @@
+
+#include "CppUTest/TestHarness.h"
+#include "LedDriver.h"
+
+TEST_GROUP(LedDriver)
+{
+    void setup()
+    {
+    }
+
+    void teardown()
+    {
+    }
+
+    void expect(const char * s)
+    {
+    }
+
+    void given(int charsWritten)
+    {
+    }
+};
+
+TEST(LedDriver, FailingTest)
+{
+    FAIL_TEST("CppUtests not implemented for LedDriver\n");
+}
---
+++ Tests/Utils/Random/CppUtest/main.cpp
@@ -0,0 +1,6 @@
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int argc, const char * argv[])
+{
+    return RUN_ALL_TESTS(argc, argv);
+}
