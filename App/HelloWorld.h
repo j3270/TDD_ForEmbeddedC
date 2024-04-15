8100aef746fc31c0c49571dc09b2bbb82a903272
Justin Jordan <justinjordan1978@gmail.com>
2024-04-14 17:15:17 -0700

    Add circular buffer and hello word fn to exercise it

---
+++ App/HelloWorld.h
@@ -0,0 +1,18 @@
+/**
+ * @file HelloWorld.h
+ * @author your name (you@domain.com)
+ * @brief HelloWorld fns for modules, aka printf testing
+ * @version 0.1
+ * @date 2024-04-14
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef HELLO_WORLD_H_
+#define HELLO_WORLD_H_
+
+void Random_HelloWorld(void);
+void CircularBuffer_HelloWorld(void);
+
+#endif /* HELLO_WORLD_H_ */
--- App/main.c
+++ App/main.c
@@ -11,34 +11,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include "Random.h"
-#include "Macros.h"
+#include "HelloWorld.h"
 
 int main(int argc, char * argv[])
 {
-    const int lower = -100;
-    const int upper = 100;
-    const size_t max_idx = 10000;
-    int random_val, min, max, avg = 0;
-
-    printf("Generate csv file of random numbers.\n");
-
-    FILE * fp;
-    fp = fopen("./Random.csv", "w+");
-    for(size_t idx = 0; idx < max_idx; idx++)
-    {
-        random_val = Random_Int_InRange(lower, upper);
-
-        min = MIN(min, random_val);
-        max = MAX(max, random_val);
-        avg += random_val;
-
-        fprintf(fp, "%zu,%d\n", idx, random_val);
-    }
-    fclose(fp);
-
-    printf("Min val = %d\n", min);
-    printf("Max val = %d\n", max);
-    printf("Avg val = %f\n", avg/(max_idx * 1.0f));
-    return 0;
+    Random_HelloWorld();
+    CircularBuffer_HelloWorld();
 }
+
